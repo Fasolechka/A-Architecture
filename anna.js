3c2693956412bc87e195bba4e35f42f2fc04c229
Fasolechka <lenabrush@gmail.com>
2023-03-07 01:12:40 +0600

    Add debug & add onMove() for images

--- anna.js
+++ anna.js
@@ -4,17 +4,22 @@ function onMove() {
     isMove = true;
 }
 
-if (typeof (Storage) !== "undefined") {
-    const wasMove = localStorage.getItem("is-move") === 'true';
+const wasMove = localStorage.getItem("is-move") === 'true';
 
-    const loadingOverlay = document.querySelector('.loading-overlay');
+const loadingOverlay = document.querySelector('.loading-overlay');
 
-    if (loadingOverlay) {
-        loadingOverlay.style.display = wasMove ? 'none' : 'flex';
-    }
+if (loadingOverlay) {
+    loadingOverlay.style.display = wasMove ? 'none' : 'flex';
+}
 
-    window.addEventListener('beforeunload', function () {
-        localStorage.setItem("is-move", isMove);
-    });
-};
+document.getElementById('debug').innerHTML = JSON.stringify({
+    wasMove,
+    isMove,
+    loadingOverlay,
+    typeOfStorage: typeof (Storage),
+});
+
+window.addEventListener('beforeunload', function () {
+    localStorage.setItem("is-move", isMove);
+});
 
