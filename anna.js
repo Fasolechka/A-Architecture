8e47b26a96abc5806ef423ad5f10ad0d51767e27
Fasolechka <lenabrush@gmail.com>
2023-03-07 02:17:31 +0600

    Update site

--- anna.js
+++ anna.js
@@ -1,45 +1,15 @@
-function getLogs() {
-    return localStorage.getItem("logs") || '';
-}
-function writeLog(message) {
-    localStorage.setItem("logs", getLogs() + ';' + message);
-}
-
-function clearLogs() {
-    localStorage.setItem("logs", '');
-    document.getElementById('debug').innerHTML = '';
-}
-
-let isMove = false;
-
 function onMove() {
-    writeLog('onMove()');
-    isMove = true;
+    localStorage.setItem("should-show", 'false');
 }
 
 if (typeof (Storage) !== "undefined") {
-    const wasMove = localStorage.getItem("is-move") === 'true';
-    writeLog(`was move = ${wasMove}`);
+    const shouldShow = (localStorage.getItem("should-show") || 'true') === 'true';
+    localStorage.setItem("should-show", 'true');
 
     const loadingOverlay = document.querySelector('.loading-overlay');
 
     if (loadingOverlay) {
-        writeLog(`changed style: ${wasMove ? 'none' : 'flex'}`);
-        loadingOverlay.style.display = wasMove ? 'none' : 'flex';
+        loadingOverlay.style.display = shouldShow ? 'flex' : 'none';
     }
-
-    window.addEventListener('beforeunload', function () {
-        writeLog(`beforeunload: isMove = ${isMove}`);
-        localStorage.setItem("is-move", isMove);
-    });
-
-    window.addEventListener('popstate', function () {
-        writeLog(`popstate: isMove = ${isMove}`);
-        localStorage.setItem("is-move", isMove);
-    });
 };
 
-const debugElement = document.getElementById('debug');
-if (debugElement) {
-    debugElement.innerHTML = getLogs();
-}
