e66e2c0eb1494108cec80d9c036364c8d8d5d614
Fasolechka <lenabrush@gmail.com>
2023-03-07 01:28:02 +0600

    added logs

--- anna.js
+++ anna.js
@@ -1,25 +1,40 @@
+function getLogs() {
+    return localStorage.getItem("logs") || '';
+}
+function writeLog(message) {
+    localStorage.setItem("logs", getLogs() + ';' + message);
+}
+
+function clearLogs() {
+    localStorage.setItem("logs", '');
+    document.getElementById('debug').innerHTML = '';
+}
+
 let isMove = false;
 
 function onMove() {
+    writeLog('onMove()');
     isMove = true;
 }
 
-const wasMove = localStorage.getItem("is-move") === 'true';
+if (typeof (Storage) !== "undefined") {
+    const wasMove = localStorage.getItem("is-move") === 'true';
+    writeLog(`was move = ${wasMove}`);
 
-const loadingOverlay = document.querySelector('.loading-overlay');
+    const loadingOverlay = document.querySelector('.loading-overlay');
 
-if (loadingOverlay) {
-    loadingOverlay.style.display = wasMove ? 'none' : 'flex';
+    if (loadingOverlay) {
+        writeLog(`changed style: ${wasMove ? 'none' : 'flex'}`);
+        loadingOverlay.style.display = wasMove ? 'none' : 'flex';
+    }
+
+    window.addEventListener('beforeunload', function () {
+        writeLog(`beforeunload: isMove = ${isMove}`);
+        localStorage.setItem("is-move", isMove);
+    });
+};
+
+const debugElement = document.getElementById('debug');
+if (debugElement) {
+    debugElement.innerHTML = getLogs();
 }
-
-document.getElementById('debug').innerHTML = JSON.stringify({
-    wasMove,
-    isMove,
-    loadingOverlay,
-    typeOfStorage: typeof (Storage),
-});
-
-window.addEventListener('beforeunload', function () {
-    localStorage.setItem("is-move", isMove);
-});
-
